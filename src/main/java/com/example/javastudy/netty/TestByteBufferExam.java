938231f01889cd73d118fb7b032990719ff9f87d
徐昊昱 <1242783117@qq.com>
2022-10-02 00:53:34 +0800

    fileChannel study

---
+++ src/main/java/com/example/javastudy/netty/TestByteBufferExam.java
@@ -0,0 +1,42 @@
+package com.example.javastudy.netty;
+
+
+import java.nio.ByteBuffer;
+
+/**
+ * 粘包半包分析
+ * 网络传输中当我们的数据是以数据包传输的，一开始的时候如果数据包过小他会等待数据包达到一定数量的情况下才会发送（粘包），
+ * 这样做的好处类似于mysql中的WAL机制，这样数据就会被拆分，导致比如一句完整的话被切分（半包问题）
+ * 例：比如我们传输一句话用\n切分发送
+ * hello,world\nI'm xuhaoyu\nice to meet you
+ */
+public class TestByteBufferExam {
+
+    public static void main(String[] args) {
+        ByteBuffer source = ByteBuffer.allocate(64);
+        source.put("hello,world\nI'm xuhaoyu\nnice to meet you".getBytes());
+        split(source);
+    }
+
+    private static void split(ByteBuffer source) {
+        // 切换到读模式
+        source.flip();
+        for (int i = 0; i < source.limit(); i++) {
+            // 找到一条完整信息
+            if (source.get(i) == '\n'){
+                // 换行符索引+1-position
+                int length = i + 1 - source.position();
+                // 吧这条完整信息存入心的ByteBuffer
+                ByteBuffer target = ByteBuffer.allocate(length);
+                // 从source读，像target写
+                for (int j = 0; j < length; j++) {
+                    target.put(source.get());
+                }
+                target.flip();
+                System.out.println(target.get());
+            }
+
+        }
+        source.compact();
+    }
+}
---
+++ src/main/java/com/example/javastudy/netty/TestFileChannelTransferTo.java
@@ -0,0 +1,28 @@
+package com.example.javastudy.netty;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.channels.FileChannel;
+
+public class TestFileChannelTransferTo {
+
+    public static void main(String[] args) {
+        try(
+                FileChannel form = new FileInputStream("test.txt").getChannel();
+                FileChannel to = new FileOutputStream("data.txt").getChannel();
+
+           ){
+            // 效率高，transferTo底层会采用零拷贝技术
+            long size = form.size();
+            // left 变量还剩余多少字节
+            // 因为一次只会读取2g大小的文件，所以需要分段处理
+            for(long left = size;left>0;){
+                left -= form.transferTo((size-left),left,to);
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
