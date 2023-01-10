0cce8eeb535215c858735f16b6e417aba3cbd500
徐昊昱 <1242783117@qq.com>
2023-01-10 22:22:29 +0800

    消息发送以及消息接受的流程

--- rabbit-mq/mq-consumer/src/main/java/com/demos/rabbit/RabbitConsumerApplication.java
+++ rabbit-mq/mq-consumer/src/main/java/com/demos/rabbit/RabbitConsumerApplication.java
@@ -6,6 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 
+/**消息接受
+ * 1、消费者和Broker建立TCP连接
+ * 2、消费者和Broker建立通道（channel）
+ * 3、消费者监听指定的Queue（队列）
+ * 4、当由消息到达Queue的时候将消息推送给消费者
+ * 5、消费者接受到消息返回ACK回复
+ */
 public class RabbitConsumerApplication {
 
     public static void main(String[] args) {
--- rabbit-mq/mq-provider/src/main/java/com/demos/rabbit/RabbitProviderApplication.java
+++ rabbit-mq/mq-provider/src/main/java/com/demos/rabbit/RabbitProviderApplication.java
@@ -3,6 +3,21 @@ package com.demos.rabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+/**
+ * 生产者->交换机->队列->客户端->
+ * borker:消息队列服务进程，包括Exchange和Queue
+ * Exchange：消息队列交换机，按一定规则将消息路由转发到某个队列，进行消息过滤
+ * Queue:消息队列，存储消息的队列，消息到达队列转发给指定的消费者
+ * Producer：消息生产者
+ * Consumer：消息消费者，即消费方客户端，接受MQ转发的消息
+ *
+ * 消息生产：
+ * 1、生产者和Broker建立TCP连接
+ * 2、生产者和Broker建立通道
+ * 3、生产者通过通道消息发送给Broker，由Exchange将消息转发到指定的Queue
+ *
+ */
 @SpringBootApplication
 public class RabbitProviderApplication {
 
